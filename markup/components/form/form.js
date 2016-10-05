1c01b1761adc0c0babe02d092fe1828537c5bc28
Gaboa <dmitry.zhabko@gmail.com>
2016-10-05 17:27:44 +0300

    Some init form

---
+++ markup/components/form/form.js
@@ -0,0 +1,32 @@
+import $ from 'jquery';
+
+export let form = (function () {
+
+    function init(gameID, casinoID, userID) {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: `http://gameservice.bossgs.org/testslot/SlotService.svc/_Initialise/${userID}/${casinoID}/${gameID}`,
+                dataType: 'JSONP',
+                type: 'GET',
+                success: resolve,
+                error: reject
+            });
+        });
+    }
+
+    $('Form__submit').on('click', (event) => {
+
+        console.log('I am submited!');
+
+        event.preventDefault();
+        let gameID = $('Form__gameID').value();
+        let casinoID = $('Form__casinoID').value();
+        let userID = $('Form__userID').value();
+
+        init(gameID, casinoID, userID).then((data) => {
+            console.log('data', data);
+        });
+
+    });
+
+})();
